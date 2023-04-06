f1c0bc090619479122ce970571e403cb86976d2c
aprylle-1 <aprylle.ablaza@gmail.com>
2023-04-06 13:22:23 +0800

    merging

---
+++ unit-47.0-data-structures-algorithms/unit-47.11-sorting/bubble.js
@@ -0,0 +1,14 @@
+function bubbleSort(arr) {
+    for (let i = 0; i < arr.length; i++){
+        for (let j = i + 1; j < arr.length; j++){
+            if (arr[j] < arr[i]){
+                const temp = arr[j]
+                arr[j] = arr[i]
+                arr[i] = temp
+            }
+        }
+    }
+    return arr
+}
+
+module.exports = bubbleSort;
